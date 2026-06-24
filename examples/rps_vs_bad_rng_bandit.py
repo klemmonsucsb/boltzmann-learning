1f7c6bcddee222320f257f87cc80fe0a774f1701
Kyle303030 <78828106+Kyle303030@users.noreply.github.com>
2026-06-24 12:15:58 -0700

    Add sandbox copies for bandit-feedback experiments (no logic changes yet)

---
+++ examples/rps_vs_bad_rng_bandit.py
@@ -0,0 +1,168 @@
+import numpy as np
+from collections import OrderedDict
+
+
+class RPSVsBadRNG:
+    action_set = ["R", "P", "S"]
+
+    def __init__(self, action_sequence, action_sequence_2, sequence_change_idx, length_measurement=1):
+        """Create game
+
+        Args:
+            action_sequence: sequence of actions for player 2
+        """
+        self.action_sequence = action_sequence
+        self.action_sequence_2 = action_sequence_2
+        self.sequence_change_idx = sequence_change_idx
+        self.time_counter = 0
+        self.last_action = len(self.action_sequence) - 1
+        # create outputs with all combinations of actions
+        measurement_set = self.action_set
+        for i in range(length_measurement - 1):
+            new_measurement_set = set()
+            for m in measurement_set:
+                for a in self.action_set:
+                    new_measurement_set.add(m + a)
+            measurement_set = new_measurement_set
+        print("Measurement set with {:d} elements".format(len(measurement_set)))
+        if len(measurement_set) < 28:
+            print(measurement_set)
+        self.measurement_set = list(measurement_set)
+        # initialize measurement
+        self.measurement = length_measurement * "S"
+
+    def cost(self, p1_action, p2_action) -> float:
+        """Returns game outcome
+
+        Args:
+            p1_action: action for player 1
+            p2_action: action for player 2
+
+        Returns:
+            game result (float): -1 if player 1 wins, +1 if player 1 loses, 0 draw
+        """
+        if p1_action == p2_action:
+            # draw
+            return 0
+        if (p1_action == "R" and p2_action == "S") or (p1_action == "P" and p2_action == "R") or (
+                p1_action == "S" and p2_action == "P"):
+            return -1
+        else:
+            return +1
+
+    def get_measurement(self):
+        """get measurement for next game
+
+        Returns:
+            measurement: measurement for next game
+        """
+        return self.measurement, None
+
+    def play(self, p1_action) -> tuple[float, dict[str, float]]:
+        """Play game
+
+        Args:
+            p1_action: action for player 1
+
+        Returns: 
+            tuple[float,dict(str,float)]
+            cost (float): -1 if player 1 wins, +1 if player 1 loses, 0 draw
+            all_costs (dict[str,float]): dictionary with costs for all actions of player 1
+        """
+        # select action for player 2
+        self.time_counter += 1
+        self.last_action += 1
+        if self.last_action >= len(self.action_sequence):
+            self.last_action = 0
+        if self.time_counter < self.sequence_change_idx:
+            p2_action = self.action_sequence[self.last_action]
+        else:
+            p2_action = self.action_sequence_2[self.last_action]
+        # update measurement
+        self.measurement = self.measurement[1:] + p2_action
+        # cost for given action of player 1
+        cost = self.cost(p1_action, p2_action)
+        # costs for all actions of player 1
+        all_costs = OrderedDict([(a, self.cost(a, p2_action)) for a in self.action_set])
+        return cost, all_costs, p2_action
+
+
+if __name__ == '__main__':
+    from sklearn import svm, neural_network
+    from examples.benchmark_methods import BayesianEstimator, SklearnModel
+    from examples.simulation_utils.utils import GamePlay
+    from learning_games import LearningGame
+    import pickle
+
+    M: int = 101_000  # the total number of rounds to play the game
+    length_measurement: int = 5  #
+    switch_time = 20_000
+    beta_values = [1e-2, 1e-1, 1e0, 1e1]
+    lambda_values = [1e1, 1e0, 1e-1, 1e-2, 1e-3, 1e-4, 0.]
+    label_to_action = {'R': 'P', 'P': 'S', 'S': 'R'}
+    rng = np.random.default_rng(11)
+    action_sequence = rng.permutation(["R", "P", "S"] * 50)
+    rng2 = np.random.default_rng(7)
+    action_sequence2 = rng2.permutation(["R", "P", "S"] * 50)
+
+    # benchmark model parameters
+    # whether to learn a map from measurements to label (True) or estimate the cost associated with each action (False)
+    measurement_to_label: bool = False
+    # the window size H of recent data
+    data_window: int = 1_000
+    # the frequency at which the model is updated. More updates requires more time
+    update_freq: int = 1_000
+    # the mapping from labels to actions when learning measurement_to_label
+    label_action_policy = {0: 0, 1: 1}
+    # hidden layer sizes for NN/MLP
+    hidden_layer_sizes = (30, 30)
+    # the max number of training iterations for the NN
+    max_train_iter = 500
+    # kernel choice for the SVM
+    svm_kernel = 'rbf'
+    # random state for the sklearn models
+    random_state = 1
+
+    print("action sequence :", action_sequence)
+    game = RPSVsBadRNG(action_sequence=action_sequence, length_measurement=length_measurement,
+                          action_sequence_2=action_sequence2, sequence_change_idx=switch_time)
+
+    bayesian1 = BayesianEstimator(action_set=game.action_set, measurement_set=game.measurement_set)
+    bayesian2 = BayesianEstimator(action_set=game.action_set, measurement_set=game.measurement_set)
+
+    bayesian = BayesianEstimator(action_set=game.action_set, measurement_set=game.measurement_set)
+    methods = [bayesian]
+    # for b, l in itertools.product(beta_values, lambda_values):
+    for b, l in zip(beta_values, lambda_values):
+        lg = LearningGame(game.action_set, measurement_set=game.measurement_set,
+                            decay_rate=l, inverse_temperature=b, seed=0)
+        lg.reset()
+        methods.append(lg)
+
+    if measurement_to_label:
+        nn_model = neural_network.MLPClassifier(random_state=random_state,
+                                                max_iter=max_train_iter,
+                                                hidden_layer_sizes=hidden_layer_sizes)
+        svm_model = svm.SVC(kernel=svm_kernel)
+    else:
+        nn_model = neural_network.MLPRegressor(random_state=random_state,
+                                               max_iter=max_train_iter,
+                                               hidden_layer_sizes=hidden_layer_sizes)
+        svm_model = svm.SVC(kernel='rbf')
+    svm = SklearnModel(window_size=data_window, action_set=game.action_set, measurement_set=game.measurement_set,
+               raw_measurement=False, measurement_to_label=measurement_to_label, finite_measurement=True, policy_map=label_to_action,
+              update_frequency=update_freq, model=svm_model)
+    mlp = SklearnModel(window_size=data_window, action_set=game.action_set, measurement_set=game.measurement_set,
+               raw_measurement=False, measurement_to_label=measurement_to_label, finite_measurement=True, policy_map=label_to_action,
+              update_frequency=update_freq, model=nn_model)
+    methods.append(svm)
+    methods.append(mlp)
+    methods.reverse()
+
+    gp = GamePlay(decision_makers=methods,
+                  game=game,
+                  horizon=M,
+                  disp_results_per_iter=int(M/10),
+                  binary_cont_measurement=False,
+                  store_energy_hist=False)
+    gp.play_games(f'../data/rps_{M}_{str(measurement_to_label)}')
---
+++ examples/simulation_utils/utils_bandit.py
@@ -0,0 +1,281 @@
+import itertools
+import time
+from copy import deepcopy
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import pickle
+
+
+def plot_simulation_results(iters, costs, average_costs, entropies, average_cost_bounds=None, title_prefix=""):
+    """
+    Plots the simulation results including costs, average costs, entropy, and average cost bounds.
+
+    Parameters:
+    - iters: List or array of iteration indices.
+    - costs: List or array of costs at each iteration.
+    - average_costs: List or array of average costs up to each iteration.
+    - entropies: List of players' array of entropy values at each iteration.
+    - average_cost_bounds: (Optional) List or array of average cost bounds up to each iteration.
+    - title_prefix: (Optional) Prefix string for plot titles.
+    """
+    slc = slice(int(.95 * len(iters)), len(iters))
+
+    fig, ax = plt.subplots(1, 2, figsize=(14, 7))
+
+    # Plot costs, average cost, and entropy, from start
+    ax_twin = ax[0].twinx()
+    lines = ax[0].plot(iters, costs, '.', label='cost', color='tab:blue')
+    lines += ax[0].plot(iters, average_costs, '-', label='average cost', color='tab:orange')
+    if average_cost_bounds is not None:
+        lines += ax[0].plot(iters, average_cost_bounds, '-', label='average cost bound', color='tab:cyan')
+    # for idx, entropy in enumerate(entropies):
+    #     lines += ax_twin.plot(iters, entropy, '.', label='entropy: P{}'.format(idx+1), color='tab:green')
+
+    ax[0].grid()
+    ax[0].set_xlabel('t')
+    ax[0].set_ylabel('cost')
+    ax[0].set_ylim((-1.1, 1.1))
+    ax_twin.set_ylabel('entropy')
+    ax[0].set_title(f"{title_prefix} Costs, Average Costs, and Entropy (Start)")
+    ax_twin.legend(lines, [line.get_label() for line in lines])
+
+    # Plot costs, average cost, and entropy, skipping start
+    ax_twin = ax[1].twinx()
+    lines = ax[1].plot(iters[slc], average_costs[slc], '-', label='average cost', color='tab:orange')
+    if average_cost_bounds is not None:
+        lines += ax[1].plot(iters[slc], average_cost_bounds[slc], '-', label='average cost bound', color='tab:cyan')
+    for idx, entropy in enumerate(entropies):
+        lines += ax_twin.plot(iters[slc], entropy[slc], '.', label='entropy: P{}'.format(idx+1), color='tab:green')
+
+    ax[1].grid()
+    ax[1].set_xlabel('t')
+    ax[1].set_ylabel('cost')
+    ax_twin.set_ylabel('entropy')
+    ax[1].set_title(f"{title_prefix} Costs, Average Costs, and Entropy (End)")
+    ax_twin.legend(lines, [line.get_label() for line in lines])
+
+    ax_twin.set_zorder(ax[0].get_zorder() + 1)
+    fig.tight_layout()
+    plt.show()
+
+
+def plot_comparison(iters, methods: list, costs: list, average_costs: list, entropies: list,
+                    average_cost_bounds=None, title_prefix="", ax=None):
+    """
+    Plots the simulation results including costs, average costs, entropy, and average cost bounds.
+    # TODO: clean up this doc
+    Parameters:
+    - iters: List or array of iteration indices.
+    - methods: the method names for the legend labels
+    - costs: List of arrays of costs at each iteration.
+    - average_costs: List of arrays of average costs up to each iteration.
+    - entropies: List of players' array of entropy values at each iteration.
+    - average_cost_bounds: (Optional) List of arrays of average cost bounds up to each iteration.
+    - title_prefix: (Optional) Prefix string for plot titles.
+    - ax (Optional): the axis on which to plot
+    """
+
+    if average_cost_bounds is None:
+        average_cost_bounds = [None for _ in average_costs]
+
+    if ax is None:
+        fig, ax = plt.subplots(1, 1, figsize=(10, 7))
+
+    # Plot costs, average cost, and entropy, from start
+    for label, cost, average_cost, average_cost_bound in zip(methods, costs, average_costs, average_cost_bounds):
+        print(label)
+        # lines = ax.plot(iters, cost, '.', label='cost | {}'.format(label))
+        lines = ax.plot(iters, average_cost, '-', label='average cost | {}'.format(label))
+        if average_cost_bound is not None:
+            lines += ax.plot(iters, average_cost_bound, '-', label='average cost bound | {}'.format(label))
+
+        ax.grid()
+        ax.set_xlabel('t')
+        ax.set_ylabel('cost')
+        ax.set_ylim((-1.1, 1.1))
+        ax.set_title(f"{title_prefix}")
+
+    ax.scatter(range(len(entropies[0])), entropies[0], label='LearningGame entropy', s=1)
+    ax.legend()
+    # fig.tight_layout()
+    plt.show()
+
+
+def generate_probabilities_matrix(prob_matrix: np.array) -> (list[str], np.array):
+    """
+    Given probabilities (row is a sample, and columns correspond to independent probabilities), compute the probability
+    of each possible outcome. Assumes a binary setting, resulting in 2^{n_columns} possible outcomes.
+    Args:
+        prob_matrix (np.array): contains the
+
+    Returns:
+
+    """
+    n = prob_matrix.shape[1]  # Number of probabilities in each set
+
+    # Generate all possible outcomes (binary strings of length n)
+    outcomes = np.array(list(itertools.product([0, 1], repeat=n)))
+
+    # Convert the probabilities list to a NumPy array
+    prob_matrix = np.array(prob_matrix)
+
+    # Calculate the probability of each outcome for each set of probabilities
+    outcome_probabilities = []
+    for probs in prob_matrix:
+        # Compute the probabilities for this set
+        probs_matrix = probs * outcomes + (1 - probs) * (1 - outcomes)
+        outcome_probs = np.prod(probs_matrix, axis=1)
+        outcome_probabilities.append(outcome_probs)
+
+    outcomes_strings = ["".join(outcome.astype(str)) for outcome in outcomes]
+    return outcomes_strings, np.array(outcome_probabilities)
+
+
+class GamePlay:
+    """Creates a game for each decision maker to play. Runs in series, but could be parallelized."""
+    def __init__(self, decision_makers: list, game, horizon: int,
+                 disp_results_per_iter: int, store_energy_hist: bool,
+                 time_index: np.ndarray = None, binary_cont_measurement: bool = False):
+        """
+        Args:
+            decision_makers (list): decision-making algorithms to be tested
+            game: an instance of a game to play # TODO: should specify type
+            horizon (int): the horizon of the game
+            time_index (np.ndarray, optional): array of the times associated with each index.
+                        Default is None, in which case it uses an index based on horizon (i.e. 1 unit time between each
+                        sample.
+            disp_results_per_iter (int): how often to output current gameplay info (1 is every step)
+            binary_cont_measurement (bool): whether to plot current policy at each disp_results_per_iter. Only supported
+                                            for binary continuous measurement settings.
+            store_energy_hist (bool): whether to keep track of the energy at each time step. Can take significant memory
+        """
+        self.decision_makers = decision_makers  # each decision maker will
+        self.game = game
+        self.horizon = horizon
+        if time_index is not None:
+            self.time_iter = time_index[:-1]
+        else:
+            self.time_iter = range(self.horizon - 1)
+        self.disp_results_per_iter = disp_results_per_iter
+        self.binary_cont_measurement = binary_cont_measurement
+        self._store_energy_hist = store_energy_hist
+
+    def play_games(self, save_to: str):
+        """ Play all the games
+        Args:
+            save_to (str): directory to save outputs to
+
+        """
+        for decision_maker in self.decision_makers:
+            game_i = deepcopy(self.game)  # create a copy of the game
+            algorithm_name = decision_maker.__class__.__name__
+            if algorithm_name == 'SklearnModel':
+                algorithm_name = decision_maker.model.__class__.__name__
+            print('Playing', algorithm_name)
+            if algorithm_name == 'LearningGame':
+                algorithm_name = r'Boltzmann Learning | $\lambda={:.1e}, \beta={:.1e}$'\
+                    .format(decision_maker.decay_rate, decision_maker.inverse_temperature)
+            output = self._play_game(game_i, decision_maker)
+            with open(save_to + f'/{algorithm_name}.pkl', 'wb') as f:
+                pickle.dump(output, file=f)
+        print('Finished playing games.')
+
+    def _play_game(self, game, decision_maker) -> dict:
+        """
+        Play a particular game. Helper function for self.play_games()
+        Args:
+            game: the game to play  #TODO: needs typing
+            decision_maker (decision_maker.DecisionMaker): the agent to make decisions
+
+        Returns: (dict) containing a summary of relevant statistics from the simulation
+
+        """
+
+        costs = np.zeros(self.horizon)
+        expected_costs = np.zeros(self.horizon)
+        cost_bounds = np.zeros(self.horizon)
+        entropy = np.zeros(self.horizon)
+        p1_action = []
+        probs = []
+        energies = dict()
+        start_time = time.perf_counter()
+        for idx, time_i in enumerate(self.time_iter):
+            # Play
+            measurement, raw_measurement = game.get_measurement()
+            action, prob, entropy[idx] = decision_maker.get_action(measurement=measurement, time=time_i,
+                                                                   raw_measurement=raw_measurement)
+            # print('action time {}'.format(time.perf_counter() - st))
+            costs[idx], all_costs, opponent_action = game.play(action)
+            p1_action.append(action)
+            probs.append(prob)
+            if self._store_energy_hist: # TODO: uncomment
+                if hasattr(decision_maker, 'energy'):
+                    # TODO: should be a cleaner way to do this but gets around immutability of dict
+                    for key, val in decision_maker.energy.items():
+                        for key2, val2 in val.items():
+                            total_key = key + str(key2)
+                            if total_key not in energies.keys():
+                                energies[total_key] = []
+                            energies[total_key].append(val2)
+            if prob is None:
+                expected_costs[idx] = None
+            else:
+                expected_costs[idx] = (np.array([v for k, v in all_costs.items()]) * prob).sum()
+            # Learn
+            decision_maker.update_energies(measurement=measurement, costs=all_costs, action=action,
+                                           raw_measurement=raw_measurement,
+                                           time=time_i, action_cost=costs[idx], opponent_action=opponent_action, idx=idx)
+            elapsed_time = time.perf_counter() - start_time
+            # Store regret
+            try:
+                # TODO: ideally all decision makers will have regret function
+                (_, _, cost_bounds[idx], _, _, _, _, _) = decision_maker.get_regret(display=False)
+            except Exception:
+                pass
+
+            # Output
+            if (idx % self.disp_results_per_iter == 0) or (idx == self.horizon - 1):
+                print(idx)
+                if idx > 0:
+                    print('Elapsed time: {} | Average per step time: {}'.format(elapsed_time, elapsed_time / idx))
+
+                if self.binary_cont_measurement:
+                    plot_binary_policy(decision_maker=decision_maker, game=game, time_idx=time_i)
+        elapsed_time = time.perf_counter() - start_time
+        print('Total time: {} | Average per step time: {}'.format(elapsed_time, elapsed_time/self.horizon))
+        iters = range(self.horizon)
+        moving_average_costs = pd.Series(costs).rolling(window=1000, min_periods=1).mean()
+        return dict(iters=iters, costs=costs, cost_bounds=cost_bounds, expected_costs=expected_costs,
+                    average_costs=moving_average_costs, entropy=entropy, probs=probs, energies=energies)
+
+
+def plot_binary_policy(decision_maker, game, time_idx):
+    """Plot the action selection probabilities for a binary setting (i.e. |A|=2)."""
+    fig, ax = plt.subplots()
+    p = []
+    meas_range = np.arange(0.1, 1.0, step=.01).round(2)
+    pos_res = []
+    bin_counts = []
+    meas_seq_probs = game.measurement_sequence[:, 1].round(2)
+    for measurement_m in meas_range:
+        (_, prob_m, _) = decision_maker.get_action(measurement={'0': 1 - measurement_m, '1': measurement_m}, time=time_idx)
+        p.append(prob_m[1])
+        instance_count = (meas_seq_probs == measurement_m).sum()
+        bin_counts.append(instance_count)
+        pos_res.append(np.sum(game.opponent_action_sequence[meas_seq_probs == measurement_m] == 1) / instance_count)
+    p = np.array(p)
+    pos_res = np.array(pos_res)
+
+    ax[0].plot(meas_range, p, label='Boltzmann')
+    ax[0].plot(meas_range, pos_res, label='percent malicious')
+    ax[0].legend()
+    ax[0].set_ylabel('Prob action="mark malicious"')
+    ax[0].set_ylim(0, 1.1)
+    ax[0].set_xlim(0, 1.)
+    ax[1].set_xlabel('Measurement (prob malicious)')
+    ax[0].grid()
+    ax[1].scatter(meas_range, bin_counts)
+    ax[1].set_ylabel('Bin counts')
+    plt.show()
